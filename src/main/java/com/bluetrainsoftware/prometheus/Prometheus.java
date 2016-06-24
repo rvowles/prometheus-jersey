e32b925b4ff62c86c4bf3e088ca62477722e68c3
Richard Vowles <richard@bluetrainsoftware.com>
2016-06-25 01:00:53 +1200

    initial commit

---
+++ src/main/java/com/bluetrainsoftware/prometheus/Prometheus.java
@@ -0,0 +1,22 @@
+package com.bluetrainsoftware.prometheus;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Defines a Jersey API as requiring a Histogram wrapper and specifies the metric
+ * it will be exposed as and the help provided. This needs to be unique otherwise
+ * Prometheus has conniptions.
+ *
+ * @author Richard Vowles - https://plus.google.com/+RichardVowles
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Prometheus {
+	String name();
+	String help();
+}
